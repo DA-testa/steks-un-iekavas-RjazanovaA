2f5fb5f0cf5b452af85613a774f95fa53ae82a15
RjazanovaA <125595911+RjazanovaA@users.noreply.github.com>
2023-02-19 22:00:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,33 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i+1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching (opening_brackets_stack[-1].char,next):
+                print("ir tukss")
+                return i+1
+     if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+     return "success"
+              
+     pass
+           
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    if text[0]=="I":
+        text=input()
+        mismatch=find_mismatch(text)
+        print(mismatch)
     # Printing answer, write your code here
 
 
