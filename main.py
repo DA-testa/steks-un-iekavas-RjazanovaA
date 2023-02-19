1053469db88e41ed02b3585dc7955e095dedc8b4
RjazanovaA <125595911+RjazanovaA@users.noreply.github.com>
2023-02-20 00:01:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def find_mismatch(text):
       
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
                 return i+1
             opening_brackets_stack.pop()
